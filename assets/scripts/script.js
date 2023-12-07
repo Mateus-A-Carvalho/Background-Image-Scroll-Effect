1e00c78f70bb2f69e117e873535b00c87976a1dc
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-12-06 21:35:13 -0300

    Added animation fade-in-top

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,6 +3,5 @@ const bgSectionEl = document.querySelector('#bgc-scroll')
 window.addEventListener("scroll", () => {
   bgSectionEl.style.opacity = 1 - window.scrollY / 800;
   bgSectionEl.style.backgroundSize = 150 - window.scrollY / 12 + "%";
-
 })
 
